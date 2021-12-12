010f205b105fa59feeedbfce8db350e280fbe3d3
LATBauerdick <github@bauerdick.org>
2021-12-12 11:26:20 -0600

    2021-12-11

--- qmk/keyboards/dz60/keymaps/latbauerdick/keymap.c
+++ qmk/keyboards/dz60/keymaps/latbauerdick/keymap.c
@@ -2,10 +2,15 @@
 
 enum alt_layers {
   _COLEMAK,
-  /* _QWERTY, */
+  _QWERTY,
   _RAISE,
   _NAV,
   _ADJ
+};
+
+enum dz60_keycodes {
+  COLEMAK = SAFE_RANGE,
+  QWERTY
 };
 
 #define RAISE    MO(_RAISE)
@@ -33,6 +38,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  KC_LSFT, LSF_Z  , KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_SLSH, KC_K,    KC_H,    KC_COMM, KC_DOT,           RSF_ENT,          ADJ_ENT,
           KC_CAPS, ALT_ESC, GUI_ENT,          GUI_SPC,          RSF_SPC,          RSE_SPC,          RGU_SPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
+    [_QWERTY] = LAYOUT(
+          KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0   , KC_MINS, KC_EQL , KC_GRV , KC_BSPC,
+          KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_Y,    KC_U   , KC_I,    KC_O,    KC_P,    KC_Y,    KC_LBRC, KC_RBRC, KC_BSPC,
+          CTL_ESC, NAV_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_SCLN,          KC_ENT,
+ KC_LSFT, LSF_Z  , KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          RSF_ENT,          ADJ_ENT,
+          KC_CAPS, ALT_ESC, GUI_ENT,          GUI_SPC,          RSF_SPC,          RSE_SPC,          RGU_SPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    ),
     [_RAISE] = LAYOUT(
          _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_F13 , _______, \
          KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_RBRC, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_LBRC, KC_RBRC, _______,          \
@@ -49,9 +61,29 @@ _______, _______, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL , KC_LPRN, KC_BSLS, KC_RPRN,
     ),
     [_ADJ] = LAYOUT(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
-        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, RESET,
+        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, COLEMAK, QWERTY , _______, _______, RESET,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______,          _______,          _______,          _______,          _______, KC_BRMD, KC_VOLD, KC_VOLU, KC_BRMU
     ),
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        print("mode just switched to qwerty and this is a huge string\n");
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case COLEMAK:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_COLEMAK);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
+
