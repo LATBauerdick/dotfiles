568cf2be5f367b52325825dad885e00033f33a25
LATBauerdick <github@bauerdick.org>
2022-01-24 18:09:36 -0600

    2022-01-24

--- qmk/keyboards/massdrop_alt/keymaps/latbauerdick/keymap.c
+++ qmk/keyboards/massdrop_alt/keymaps/latbauerdick/keymap.c
@@ -40,10 +40,10 @@ enum alt_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_65_ansi_blocker( /* Colemak wide layout */
-        KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_EQL , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_BSPC, KC_BSPC, \
-        KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_LBRC, KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_QUOT, KC_BSLS, KC_HOME, \
-        CTL_ESC, NAV_A  , KC_R   , KC_S   , KC_T   , KC_G   , KC_RBRC, KC_M   , KC_N   , KC_E   , KC_I   , NAV_O  ,          KC_ENT , KC_PGUP, \
-        LSF_Z  , KC_X   , KC_C   , KC_D   , KC_V   , KC_Z   , KC_SLSH, KC_K   , KC_H   , KC_COMM, KC_DOT ,          RSF_SLS, KC_ENT , KC_ENT , \
+        KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, KC_BSPC, \
+        KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_LBRC, KC_J   , KC_L   , KC_U   , KC_Y   , KC_QUOT, KC_RBRC, KC_BSLS, KC_HOME, \
+        CTL_ESC, NAV_A  , KC_R   , KC_S   , KC_T   , KC_G   , KC_SCLN, KC_M   , KC_N   , KC_E   , KC_I   , KC_O   ,          KC_ENT , KC_PGUP, \
+        LSF_Z  , KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_SLSH, KC_K   , KC_H   , KC_COMM, KC_DOT ,          RSF_SLS, KC_UP  , KC_PGDN, \
         KC_LCTL, KC_LALT, GUI_DEL,                            RSE_SPC,                            GUI_RET, ADJ_LFT, KC_DOWN, KC_UP  , KC_RGHT  \
     ),
     [_QWERTY] = LAYOUT_65_ansi_blocker(
@@ -68,18 +68,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                            KC_0   , KC_DOT , _______, _______, _______  \
     ),
     [_RAISE] = LAYOUT_65_ansi_blocker(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_COLN, _______, _______, _______, \
+        KC_GRV , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 , _______, _______, \
+        _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_COLN, KC_GRV , _______, _______, \
         _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , XXXXXXX, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   ,          _______, _______, \
         _______, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL , KC_LPRN, XXXXXXX, KC_RPRN, KC_QUOT, _______, _______,          _______, _______, _______, \
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______  \
     ),
     [_NAV] = LAYOUT_65_ansi_blocker(
         KC_GESC, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0),S(KC_MINS),S(KC_EQL),KC_BSPC,KC_BSPC, \
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_J), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, \
-        _______, XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,          _______, KC_BRMU, \
-        _______, G(KC_X), G(KC_C), G(KC_D), G(KC_V), G(KC_Z), KC_BSPC, KC_LEFT, XXXXXXX, _______, _______,          _______, _______, KC_BRMD, \
-        _______, _______, _______,                            _______,                            _______, _______, KC_VOLD, KC_VOLU, _______  \
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RBRC, S(KC_J), XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_BSLS, _______, \
+        _______, XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_COLN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT ,          _______, KC_BRMU, \
+        _______, G(KC_Z), G(KC_X), G(KC_C), G(KC_D), G(KC_V), KC_BSPC, KC_BSPC, KC_LEFT, XXXXXXX, XXXXXXX,          _______, KC_VOLU, KC_BRMD, \
+        _______, _______, _______,                            _______,                            _______, _______, _______, KC_VOLD, _______  \
     ),
     [_ADJ] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
