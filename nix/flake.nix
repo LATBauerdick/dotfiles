cf08aff832e9dcac39fa207351e0771266439e60
LATBauerdick <github@bauerdick.org>
2023-02-22 20:18:11 -0600

    2023-02-18

--- nix/flake.nix
+++ nix/flake.nix
@@ -72,12 +72,30 @@
     };
 
 
+    m1mac.bauerdic = home-manager.lib.homeManagerConfiguration {
+      # inherit pkgs;
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      modules = [ ./users/bauerdic/home.nix ];
+      extraSpecialArgs = { # pass arguments
+        withGUI = false;
+        isDesktop = true;
+      };
+    };
+    intelmac.bauerdic = home-manager.lib.homeManagerConfiguration {
+      # pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+      pkgs = import nixpkgs {
+        system = "x86_64-darwin";
+        config = { allowUnfree = true; };
+      };
+      modules = [ ./users/bauerdic/home.nix ];
+      extraSpecialArgs = { # pass arguments
+        withGUI = false;
+        isDesktop = true;
+      };
+    };
     intel.bauerdic = home-manager.lib.homeManagerConfiguration {
-     # nixosModules.home = import ./users/bauerdic/home.nix; # attr set or list
-      system = "x86_64-linux";
-      configuration.imports = [ ./users/bauerdic/home.nix ];
-      homeDirectory = "/home/bauerdic";
-      username = "bauerdic";
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      modules = [ ./users/bauerdic/home.nix ];
       extraSpecialArgs = { # pass arguments
         withGUI = false;
         isDesktop = true;
@@ -85,80 +103,15 @@
       };
     };
 
-    m1mac.bauerdic = home-manager.lib.homeManagerConfiguration {
-      system = "aarch64-darwin";
-      homeDirectory = "/home/bauerdic";
-      username = "bauerdic";
-      /* configuration.imports = [ ./users/bauerdic/home.nix ]; */
-      extraSpecialArgs = { inherit nixpkgs; };
-      extraModules = [
-        /* ./users/bauerdic/extraPackages.nix */
-      ];
-      configuration = {
-        imports = [ ./users/bauerdic/home.nix ];
-        /* home.packages = [ inputs.nixpkgs.cargo ]; */
-      };
-      /* configuration = { config, pkgs, ... }: */
-        /* let */
-        /*   overlay-unstable = final: prev: { */
-        /*     unstable = inputs.nixpkgs-unstable.legacyPackages.aarch64-darwin; */
-        /*   }; */
-        /* in */
-        /* { */
-        /*   home.packages = [ ]; */
-        /*   /1* nixpkgs.overlays = [ overlay-unstable ]; *1/ */
-        /*   nixpkgs.config = { */
-        /*     allowUnfree = true; */
-        /*     allowBroken = true; */
-        /*   }; */
-        /*   imports = [ ./users/bauerdic/home.nix ]; */
-        /* }; */
-    };
     bauerdic = self.homeConfigurations.bauerdic.activationPackage;
     defaultPackage.aarch64-darwin = self.bauerdic;
 
-    umini.bauerdic = home-manager.lib.homeManagerConfiguration {
-      system = "x86_64-linux";
-      homeDirectory = "/home/bauerdic";
-      username = "bauerdic";
-      extraSpecialArgs = { inherit nixpkgs; };
-      extraModules = [
-        /* ./users/bauerdic/extraPackages.nix */
-      ];
-      configuration = {
-        imports = [ ./users/bauerdic/home.nix ];
-      };
-    };
-
-    intelmac.bauerdic = home-manager.lib.homeManagerConfiguration {
-      system = "x86_64-darwin";
+    lima.bauerdic = home-manager.lib.homeManagerConfiguration {
       pkgs = import nixpkgs {
-        system = "x86_64-darwin";
+        system = "aarch64-linux";
         config = { allowUnfree = true; };
       };
-      username = "bauerdic";
-      homeDirectory = "/home/bauerdic";
-      configuration = {
-        imports = [
-          ./users/bauerdic/home.nix
-        ];
-      };
-    };
-    lima = {
-      bauerdic = home-manager.lib.homeManagerConfiguration {
-        system = "aarch64-linux";
-        pkgs = import nixpkgs {
-          system = "aarch64-linux";
-          config = { allowUnfree = true; };
-        };
-        username = "bauerdic";
-        homeDirectory = "/home/bauerdic";
-        configuration = {
-          imports = [
-            ./users/bauerdic/home.nix
-          ];
-        };
-      };
+      modules = [ ./users/bauerdic/home.nix ];
     };
   };
 }
