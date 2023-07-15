948cb0e007fb7a401786c6494aca5e9d7407348e
LATBauerdick <github@bauerdick.org>
2023-07-14 23:10:41 -0500

    2023-07-14

--- machines/umini.nix
+++ machines/umini.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }@args:
+{ config, pkgs, lib, ... }@args:
 let
   hostname = "umini";
 in {
