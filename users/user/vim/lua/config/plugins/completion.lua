a1c99bc2d52230490ff1f2e1cee2fdcff85b75d0
LATBauerdick <github@bauerdick.org>
2024-12-21 21:00:01 -0600

    2024-12-21

---
+++ users/user/vim/lua/config/plugins/completion.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'saghen/blink.cmp',
+    dependencies = 'rafamadriz/friendly-snippets',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = { preset = 'super-tab' },
+
+      appearance = {
+        nerd_font_variant = 'mono'
+      },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+    },
+    opts_extend = { "sources.default" },
+    completion = {
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      ghost_text = { enabled = true },
+    },
+  },
+}
