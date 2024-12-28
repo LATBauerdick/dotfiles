c1f217ddb399beefc2d07196808861072df23660
LATBauerdick <github@bauerdick.org>
2024-12-28 10:02:29 -0600

    2024-12-28

--- users/user/vim/lua/config/plugins/completion.lua
+++ users/user/vim/lua/config/plugins/completion.lua
@@ -1,4 +1,4 @@
-return {
+return { -- use blink-cmp
   {
     'saghen/blink.cmp',
     dependencies = 'rafamadriz/friendly-snippets',
@@ -8,6 +8,7 @@ return {
     ---@type blink.cmp.Config
     opts = {
       keymap = { preset = 'super-tab' },
+      -- keymap = { preset = 'enter' },
       appearance = {
         nerd_font_variant = 'mono'
       },
@@ -15,10 +16,23 @@ return {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
     },
-    opts_extend = { "sources.default" },
+    -- opts_extend = { "sources.default" },
     completion = {
+      keyword = { range = 'prefix' },
       documentation = { auto_show = true, auto_show_delay_ms = 500 },
-      ghost_text = { enabled = true },
+      ghost_text = { enabled = false },
     },
   },
 }
+-- return {
+--
+--   { -- Autocompletion
+--     'hrsh7th/nvim-cmp',
+--     dependencies = { 'saadparwaiz1/cmp_luasnip' },
+--     config = require("config.plugins.overrides.autocompletion").cmpsetup,
+--   },
+--   { "hrsh7th/cmp-buffer", },
+--   { "hrsh7th/cmp-path", },
+--   { "hrsh7th/cmp-cmdline", },
+--   { "hrsh7th/cmp-nvim-lua", },
+-- }
