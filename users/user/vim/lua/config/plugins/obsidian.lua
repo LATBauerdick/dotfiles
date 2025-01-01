8f81c41741f8e49c9dffe35ac8bf7cc7907a2b19
LATBauerdick <github@bauerdick.org>
2025-01-01 13:13:18 -0600

    2025-01-01

--- users/user/vim/lua/config/plugins/obsidian.lua
+++ users/user/vim/lua/config/plugins/obsidian.lua
@@ -1,19 +1,23 @@
 return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
+  lazy = false,
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
   --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
   --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
+  --   "BufReadPre " .. vim.fn.expand "~" .. "/Notes/Notes/*.md",
+  --   "BufNewFile " .. vim.fn.expand "~" .. "/Notes/Notes/*.md",
   -- },
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    -- "preservim/vim-markdown",
   },
   opts = {
     workspaces = {
@@ -22,23 +26,46 @@ return {
         path = "~/Notes/Notes",
       },
     },
-    -- daily_notes = {
-    --   folder = "Notes/DN",
-    --   -- Optional, if you want to change the date format for the ID of daily notes.
-    --   date_format = "%Y-%m-%d",
-    --   -- Optional, if you want to change the date format of the default alias of daily notes.
-    --   alias_format = "%B %-d, %Y",
-    --   -- Optional, default tags to add to each new daily note created.
-    --   default_tags = { "DN" },
-    --   -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-    --   template = nil
-    -- },
-    -- -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
-    -- completion = {
-    --   -- Set to false to disable completion.
-    --   nvim_cmp = false,
-    --   -- Trigger completion at 2 chars.
-    --   min_chars = 2,
+    daily_notes = {
+      folder = "DN",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y%m%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "DN" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil
+    },
+    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+    completion = {
+      nvim_cmp = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+    open_app_foreground = true,
+    -- mappings = {
+    --   -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+    --   ["gf"] = {
+    --     action = function()
+    --       return require("obsidian").util.gf_passthrough()
+    --     end,
+    --     opts = { noremap = false, expr = true, buffer = true },
+    --   },
+    --   -- Toggle check-boxes.
+    --   ["<leader>ch"] = {
+    --     action = function()
+    --       return require("obsidian").util.toggle_checkbox()
+    --     end,
+    --     opts = { buffer = true },
+    --   },
+    --   -- Smart action depending on context, either follow link or toggle checkbox.
+    --   ["<cr>"] = {
+    --     action = function()
+    --       return require("obsidian").util.smart_action()
+    --     end,
+    --     opts = { buffer = true, expr = true },
+    --   }
     -- },
 
   },
