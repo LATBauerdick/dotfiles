76b1bf5eb15203a0b0eb095fb70ec17fb6af713e
LATBauerdick <github@bauerdick.org>
2025-01-06 19:59:50 -0600

    2025-01-06

--- darwin/darwinMasApps.nix
+++ darwin/darwinMasApps.nix
@@ -3,17 +3,11 @@
        "Blackmagic Disk Speed Test" = 425264550;
   #       "Bsky" = 6444370199;
        "Day One" = 1055511498;
-       "FruitJuice" = 671736912;
        "iA Writer" = 775737590;
        "Infuse" = 1136220934;
-       "NextDNS" = 1464122853;
        "Parcel" = 639968404;
-       "Raycast Companion" = 6738274497;
        "Reeder" = 1529448980;
        "Save to Raindrop.io" = 1549370672;
-       "SingleFile" = 6444322545;
-       "Speedtest" = 1153157709;
-  #     "Unwatched" = 6477287463;
        "Vinegar" = 1591303229;
        "WhatsApp" = 310633997;
        "WireGuard" = 1451685025;
