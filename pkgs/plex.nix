2a27bcfad288b4efaa1c058afc52327e0700dfba
LATBauerdick <github@bauerdick.org>
2025-03-01 16:01:29 -0600

    2025-03-01

--- pkgs/plex.nix
+++ pkgs/plex.nix
@@ -3,7 +3,8 @@ let
   # plexname = "umini";
   myPlex = pkgs.plex.override {
     plexRaw = pkgs.plexRaw.overrideAttrs(old: rec {
-      version = "1.41.3.9314-a0bfb8370";
+      version = "1.41.4.9463-630c9f557";
+      # version = "1.41.3.9314-a0bfb8370";
       # version = "1.41.2.9200-c6bbc1b53";
       # version = "1.41.1.9057-af5eaea7a";
       # version = "1.40.2.8395-c67dce28e";
@@ -11,7 +12,7 @@ let
       # "1.32.8.7639-fb6452ebf";  "1.32.5.7349-8f4248874"; "1.32.5.7318-0b5fb6462"; "1.32.5.7210-77f7f99fa"; "1.25.2.5319-c43dc0277";
       src = pkgs.fetchurl {
         url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-        sha256 = "sha256-ku16UwIAAdtMO1ju07DwuWzfDLg/BjqauWhVDl68/DI=";
+        sha256 = "sha256-qhTwH/Dm8JEjmBZTwR5D//pxMFcZ4OI6jhbOSRStkYA=";
       };
     });
   };
