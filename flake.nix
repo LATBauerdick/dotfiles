f9ae08b44820d4feae4c95d3c17dfe351a23e287
LATBauerdick <github@bauerdick.org>
2025-03-22 19:07:27 -0500

    2025-03-22

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
         ./machines/${name}.nix
         ./users/${user}/${user}.nix
         home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
+          # home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.${user} = import ./users/user/home.nix {
             user = user;
