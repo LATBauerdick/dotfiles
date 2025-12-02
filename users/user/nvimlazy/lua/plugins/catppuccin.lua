f5c5d20e7c3842f79dc015ef33343ca10e87e1ef
LATBauerdick <github@bauerdick.org>
2025-12-02 07:38:37 -0600

    2025-12-01 added nvimlazy

--- users/user/nvimlazy/lua/plugins/catppuccin.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "LazyVim/LazyVim",
-    enabled = false,
-    opts = {
-      colorscheme = "catppuccin-mocha",
-    },
-  },
-}
--- users/user/nvimlazy/lua/plugins/solarized.lua
+++ users/user/nvimlazy/lua/plugins/solarized.lua
@@ -3,26 +3,15 @@ return {
   {
     "maxmx03/solarized.nvim",
     enabled = true,
-    branch = "main",
-    lazy = false,
-    name = "solarized",
-    main = "solarized",
-    priority = 1000,
-    -- Display colors in a new buffer with command: `:Solarized colors`
-    -- See: https://www.lazyvim.org/configuration/plugins#%EF%B8%8F-customizing-plugin-specs
     opts = {
-      -- See: https://github.com/maxmx03/solarized.nvim/blob/main/lua/solarized/palette.lua
-      -- palette = "selenized",
-      -- variant = "spring",
-      -- See: https://github.com/maxmx03/solarized.nvim?tab=readme-ov-file#default-config
-      styles = {
-        keywords = { bold = false },
-      },
+      -- See: https://github.com/maxmx03/solarized.nvim?tab=readme-ov-file#docs
+      palette = "selenized",
+      variant = "winter",
     },
-    config = function(plugin, opts)
-      vim.o.background = "dark"
-      require(plugin.main).setup(opts)
-      vim.cmd.colorscheme = "solarized"
-    end,
+    -- config = function(plugin, opts)
+    --   -- vim.o.background = "dark"
+    --   -- require(plugin.main).setup(opts)
+    -- vim.cmd.colorscheme = "solarized"
+    -- end,
   },
 }
