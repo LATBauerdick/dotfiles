765f3ab1ff0110e8c57ffc56930d62f807023c27
LATBauerdick <github@bauerdick.org>
2026-08-29 11:40:23 -0500

    2026-08-29

--- darwin/darwinPackages.nix
+++ darwin/darwinPackages.nix
@@ -1,30 +1,10 @@
 {pkgs}:
 let systemPackages = with pkgs; [
-       # failed to install airbuddy
-#      alacritty
-       # alt-tab-macos
-       # not on this platform bitwarden
-       # marked as broken calibre
-#       discord
-       # not on this platform  firefox
-#       google-chrome
-#       grandperspective
-#       iterm2
-#       keycastr
-#       kitty
-#       mediathekview
-#       mkalias
-#       obsidian
-#       raycast
-#       slack
-#      tailscale
-#       xld
-       # does not install xquartz
-#       zoom-us
 
-      darwin.linux-builder
+       darwin.linux-builder
        vim
        git
+
     ];
     fontsPackages = [
           #"FiraCode"
